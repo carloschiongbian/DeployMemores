33044306b99a5673f77e44caf7afc082a4d32e25
JulCbn <62123779+carloschiongbian@users.noreply.github.com>
2023-02-05 20:54:14 +0800

    Update get_users.py

--- flask-server/controllers/get_users.py
+++ flask-server/controllers/get_users.py
@@ -3,16 +3,18 @@ from models.users import Users, users_schema
 from flask.json import jsonify
 from auth.auth import is_authenticated, is_admin
 
+
 def get_users():
 
     user_id = session.get("user_id")
-    #Check if session exist
+    # Check if session exist
     if not is_authenticated(user_id):
         return jsonify({"error": "Unauthorized"}), 401
 
-    #Check if role is admin
+    # Check if role is admin
     if not is_admin(user_id):
         return jsonify({"error": "Unauthorized"}), 401
 
-    users = Users.query.filter_by(role='user',is_deleted=0).with_entities(Users.id, Users.uname, Users.fname, Users.lname, Users.role, Users.email ,Users.created_at)
+    users = Users.query.filter_by(role='user', is_deleted=0).with_entities(
+        Users.id, Users.uname, Users.fname, Users.lname, Users.role, Users.email, Users.created_at)
     return users_schema.jsonify(users)
